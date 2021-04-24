bc3e64917a968b93a8edb5fab7d5e66cc7ccccf2
thememesareallreal <83092946+thememesareallreal@users.noreply.github.com>
2021-04-24 00:07:03 -0400

    Add files via upload

---
+++ Newton'sCradle.py
@@ -0,0 +1,75 @@
+import time
+import os
+
+while 2 == 2:
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
+    print("""
+
+    ╔═════╤═╤═╤═╤═╤═╤═════╗
+    ║     │ │ │ │ │  \    ║
+    ║     │ │ │ │ │   \   ║
+    ║     │ │ │ │ │    \  ║
+    ║     │ │ │ │ │     \ ║
+    ║     │ │ │ │ │      O║
+    ║     O O O O O       ║
+    ║                     ║
+
+       Have a nice day.""")
+    print("     The time is: " + current_time + ".")
+    time.sleep(0.3)
+    os.system('cls')
+
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
+    print("""
+
+    ╔═════╤═╤═╤═╤═╤═╤═════╗
+    ║     │ │ │ │ │ │     ║
+    ║     │ │ │ │ │ │     ║
+    ║     │ │ │ │ │ │     ║
+    ║     │ │ │ │ │ │     ║
+    ║     │ │ │ │ │ │     ║
+    ║     O O O O O O     ║
+    ║                     ║
+
+       Have a nice day.""")
+    print("     The time is: " + current_time + ".")
+    time.sleep(0.1)
+    os.system('cls')
+
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
+    print("""
+
+    ╔═════╤═╤═╤═╤═╤═╤═════╗
+    ║    /  │ │ │ │ │     ║
+    ║   /   │ │ │ │ │     ║
+    ║  /    │ │ │ │ │     ║
+    ║ /     │ │ │ │ │     ║
+    ║O      │ │ │ │ │     ║
+    ║       O O O O O     ║
+    ║                     ║
+
+       Have a nice day.""")
+    print("     The time is: " + current_time + ".")
+    time.sleep(0.3)
+    os.system('cls')
+
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
+    print("""
+
+    ╔═════╤═╤═╤═╤═╤═╤═════╗
+    ║     │ │ │ │ │ │     ║
+    ║     │ │ │ │ │ │     ║
+    ║     │ │ │ │ │ │     ║
+    ║     │ │ │ │ │ │     ║
+    ║     │ │ │ │ │ │     ║
+    ║     O O O O O O     ║
+    ║                     ║
+
+       Have a nice day.""")
+    print("     The time is: " + current_time + ".")
+    time.sleep(0.1)
+    os.system('cls')
